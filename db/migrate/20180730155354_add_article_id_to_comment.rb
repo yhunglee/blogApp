bb3ab13ddc559a7c98ad9cc0d120e5e38612a3e0
Howard <howard@csie.io>
2018-07-30 23:55:16 +0800

    Add new column aritcle_id to comment model

---
+++ db/migrate/20180730155354_add_article_id_to_comment.rb
@@ -0,0 +1,5 @@
+class AddArticleIdToComment < ActiveRecord::Migration[5.2]
+  def change
+    add_column :comments, :article_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_29_162416) do
+ActiveRecord::Schema.define(version: 2018_07_30_155354) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_07_29_162416) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
   end
 
   create_table "users", force: :cascade do |t|
