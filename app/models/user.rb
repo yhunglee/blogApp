b9e733891c296a3dc41efe4bab2f5948bc69e06f
Howard <howard@csie.io>
2018-07-30 22:53:55 +0800

    Add relationship for model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  has_many :articles, -> { order("updated_at DESC")}, :dependent => :destroy
+  has_many :comments, -> { order("updated_at DESC")}, :dependent => :destroy
 end
