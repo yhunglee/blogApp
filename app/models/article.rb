76bf254d6373e8a46d43ee617a0930b92d5804e6
Howard <howard@csie.io>
2018-08-01 23:53:48 +0800

    fix(model): fix can't save article instance with user.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
     validates_presence_of :content
 
     has_many :comments, -> { order("id DESC")}, :dependent => :destroy
+    belongs_to :user, :foreign_key => 'author_id'
 end
