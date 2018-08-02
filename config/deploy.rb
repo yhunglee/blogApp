ffb61e4d4c3e86114a81204e67b34fea7acd66ad
Howard <howard@csie.io>
2018-08-03 01:04:21 +0800

    chore(deploy): change back to rails4 for getting secret_key

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, "git@github.com:yhunglee/blogApp.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/database.yml", "config/credentials.yml.enc", "config/master.key"
+append :linked_files, "config/database.yml", "config/credentials.yml.enc", "config/master.key", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
